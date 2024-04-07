b8aee1e0b93176df4c481392ee81b23ca97827f7
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-04-07 20:09:22 +0800

    Add listener to mouse enter event that changes row colors to random each time it triggers, also changing its opacity until it turns to black and then afterwards the opacity reverts back to 0

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-// Create main container div that will store the columns and row then append it to the body
+// Create main container div that will store the columns and rows
 let divContainer = document.createElement("div")
 divContainer.classList.add("container")
 document.body.appendChild(divContainer)
 
+// Get random colors
+let getColor = () => {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
 let inputSquares = (boxAmount) => {
+//store columns and rows to storeBox
+const storeBox = document.createElement("div") 
+storeBox.classList.add("storeBox")
 
   // Create the columns
   for (let boxColumn = 0; boxColumn < boxAmount; boxColumn++) {
        let column = document.createElement("div");
        column.classList.add("column");
     // Add a row after a column is created
-    for (let boxRow = 1; boxRow <= boxAmount; boxRow++) {
+    for (let boxRow = 0; boxRow < boxAmount; boxRow++) {
       const sizeOfContainer = 960 / boxAmount
       let row = document.createElement("div");
       row.classList.add("row");
@@ -18,20 +31,45 @@ let inputSquares = (boxAmount) => {
       row.style.width = `${sizeOfContainer}px` 
       row.style.height = `${sizeOfContainer}px`
       // Add a background color to rows when a mouse hovers on it to simulate a sketch pad
-      row.addEventListener('mouseenter', () =>   row.style.backgroundColor = "black")
-      column.appendChild(row);
+      row.addEventListener('mouseenter', () =>   {
+        const getOpacity = Number(row.style.opacity)
+        row.style.backgroundColor = getColor()
+        // Increase opacity each time the event listener is triggered
+        row.style.opacity = getOpacity + .1
+        // Reset row opacity value to null after reaching the maximum value of 1
+        if (Number(row.style.opacity) === 1 ){
+            row.style.backgroundColor = "black"
+            row.style.opacity = null
+           }   
+        }
+       )
+     column.appendChild(row);
     }
-    divContainer.appendChild(column);
+   storeBox.appendChild(column)
   }
+ divContainer.appendChild(storeBox)
 }
 
-inputSquares(50)
+inputSquares(0)
 
 let btnContainer = document.createElement("div")
 btnContainer.classList.add("buttons")
-
+// Create the button for changing square size
 let inputButton = document.createElement("button")
-inputButton.textContent = "Change size"
+inputButton.textContent = "Create Square"
+inputButton.addEventListener('click', () => {
+    inputButton.textContent = "Change size"
+    let userInput = Number(prompt("Input square size (1-100): "))
+    // Validate if the userInput is between 1 or 100
+    while(userInput > 100){
+        userInput = Number(prompt("Please input a number between 1 and 100: "))
+    }
+    // Remove the storeBox class to reset the sketch pad back to 0
+    const storeBox = document.querySelector(".storeBox")
+    storeBox.remove()
+    inputSquares(userInput)
+}
+)
 
 let clearButton = document.createElement("button")
 clearButton.textContent = "Clear"
